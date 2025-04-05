2a1d4c0f4856224c1a0442089aefc4ddec6b1d78
ithurtzwhenip <sinhasahil423@gmail.com>
2025-04-05 12:16:15 +0530

    Store data using SQLModel session

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 import os
 from api.db.config import DATABASE_URL
+from sqlmodel import Session
 from .models import (
     EventModel,
     EventListSchema,
     EventCreateSchema,
     EventUpdateSchema
 )
+from api.db.session import get_session
+
 router = APIRouter()
+
 
 # Get data here
 # List View
@@ -15,7 +19,7 @@ router = APIRouter()
 @router.get("/")
 def read_events() -> EventListSchema:
     # a bunch of items in a table
-    print(os.environ.get("DATABASE_URL"),DATABASE_URL)
+    print(os.environ.get("DATABASE_URL"), DATABASE_URL)
     return {
         "results": [
             {"id": 1}, {"id": 2}, {"id": 3}
@@ -23,19 +27,25 @@ def read_events() -> EventListSchema:
         "count": 3
     }
 
+
 # SEND DATA HERE
 # create view
 # POST /api/events/
-@router.post("/")
-def create_event(payload:EventCreateSchema) -> EventModel:
+@router.post("/", response_model=EventModel)
+def create_event(payload: EventCreateSchema,
+                 session: Session = Depends(get_session)):
     # a bunch of items in a table
     data = payload.model_dump()
-    return {"id": 123,**data}
+    obj = EventModel.model_validate(data)
+    session.add(obj)
+    session.commit()
+    session.refresh(obj)
+    return {"id": 123, **data}
 
 
 # GET /api/events/12
 @router.get("/{event_id}")
-def get_event(event_id:int) -> EventModel:
+def get_event(event_id: int) -> EventModel:
     # a single row
     return {"id": event_id}
 
@@ -43,12 +53,10 @@ def get_event(event_id:int) -> EventModel:
 # Update this data
 # PUT /api/events/12
 @router.put("/{event_id}")
-def update_event(event_id:int, payload:EventUpdateSchema) -> EventModel:
+def update_event(event_id: int, payload: EventUpdateSchema) -> EventModel:
     # a single row
     data = payload.model_dump()
-    return {"id": event_id,**data}
-
-
+    return {"id": event_id, **data}
 
 # @router.delete("/{event_id}")
 # def get_event(event_id:int) -> EventModel:
