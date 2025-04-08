2a5414751c7dac6318bc3fb3ea3cc93c7224e808
ithurtzwhenip <sinhasahil423@gmail.com>
2025-04-08 17:31:44 +0530

    old

--- boot/docker-run.sh
+++ boot/docker-run.sh
@@ -6,5 +6,4 @@ cd /code
 RUN_PORT=${PORT:-8000}
 RUN_HOST=${HOST:-0.0.0.0}
 
-exec gunicorn -k uvicorn.workers.UvicornWorker -b $RUN_HOST:$RUN_PORT main:app
-
+gunicorn -k uvicorn.workers.UvicornWorker -b $RUN_HOST:$RUN_PORT main:app
