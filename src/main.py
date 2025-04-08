6d42f13d9b7c49c0b3b56b109d106c7f680d5a87
ithurtzwhenip <sinhasahil423@gmail.com>
2025-04-08 16:25:53 +0530

    fix

--- src/main.py
+++ src/main.py
@@ -1,41 +1,27 @@
-from typing import Union
-from fastapi import FastAPI
-from api.events import router as event_router
 from contextlib import asynccontextmanager
-from api.db.session import init_db
-from fastapi.middleware.cors import CORSMiddleware
+from typing import Union
 
-import os
-print("✅ Starting app with DATABASE_URL =", os.getenv("DATABASE_URL"))
+from fastapi import FastAPI
+from api.db.session import init_db
+from api.events import router as event_router
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    try:
-        print("🔥 Running init_db()")
-        init_db()
-        print("✅ init_db() complete")
-    except Exception as e:
-        print("❌ Error in init_db():", e)
+    # before app startup up
+    init_db()
     yield
-
+    # clean up
 
 
 app = FastAPI(lifespan=lifespan)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-app.include_router(event_router, prefix="/api/events")
+app.include_router(event_router, prefix='/api/events')
+# /api/events
 
 
 @app.get("/")
 def read_root():
-    print("👋 Root endpoint hit")
     return {"Hello": "World"}
-
 
 
 @app.get("/items/{item_id}")
